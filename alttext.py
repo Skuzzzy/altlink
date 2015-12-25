1965a78c81690dfe85d27cd69a3fc0d816a37c08
Dan <JamesDanielBaird@gmail.com>
2015-12-24 22:05:18 -0700

    delete method

--- alttext.py
+++ alttext.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -8,7 +8,6 @@ app = Flask(__name__)
 db_filename = 'wewlad.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(db_filename)
 db = SQLAlchemy(app)
-db.create_all()
 
 tags = db.Table('tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
@@ -19,8 +18,10 @@ class Link(db.Model):
     __tablename__ = "link"
     id         = db.Column(db.Integer, primary_key=True)
     title      = db.Column(db.String(80), index=True)
+    link       = db.Column(db.String(256))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    tags = db.relationship('Tag', secondary=tags, backref=db.backref('links', lazy='dynamic'))
+    tags = db.relationship('Tag', secondary=tags,
+                           backref=db.backref('links', lazy='dynamic'))
 
     def __init__(self, link, title):
         self.link = link
@@ -28,6 +29,8 @@ class Link(db.Model):
 
     def to_dict(self):
         return {"id": self.id, "title": self.title,
+                "title": self.title,
+                "link": self.link,
                 "created_at": str(self.created_at),
                 "tags": [tag.name for tag in self.tags]}
 
@@ -49,7 +52,7 @@ def dict_contains(dictionary, *pargs):
 
 @app.route('/')
 def index():
-    return 'Todo', 200
+    return render_template('index.html'), 200
 
 @app.route('/all', methods=['GET'])
 def all_links():
@@ -61,17 +64,18 @@ def add_link():
     print request.json
     if not dict_contains(request.json, "link", "title"):
         return 'Invalid JSON Doc', 400
-
     new_link = Link(request.json.get("link"), request.json.get("title"))
     db.session.add(new_link)
     db.session.commit()
-
     return 'Created link id:{0}'.format(new_link.id), 200
 
-@app.route('/remove/<int:id>', methods=['POST'])
-def remove_link(id):
-    print id
-    return 'Todo', 200
+@app.route('/del/<int:link_id>', methods=['POST'])
+def remove_link(link_id):
+    to_delete = Link.query.filter_by(id=link_id).first_or_404()
+    db.session.delete(to_delete)
+    db.session.commit()
+    return 'Great Success', 200
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
